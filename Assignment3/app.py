df4460590f77d46907963fcaada5bfb463b0cb8c
Ubuntu <ubuntu@ip-172-31-12-77.us-west-1.compute.internal>
2021-11-03 21:03:11 +0000

    Changed bcrypt, html, python

--- Assignment3/app.py
+++ Assignment3/app.py
@@ -35,6 +35,47 @@ def welcome():
     return send_from_directory('static','welcome.html')
 
 
+# Registers a User
+@app.route('/register', methods=['POST'])
+def registerAccount():
+    username = request.form['username']
+    password = request.form['password']
+
+    #data = request.get_json()
+    #username = data['username']
+    #password = data['password']
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM users WHERE username = %s;"),(username,))
+    userPass = db.fetchone()
+
+    if userPass != None:
+        db.close()
+
+        return("false")
+    
+    salted = bcrypt.hashpw( bytes(password,  'utf-8' ) , bcrypt.gensalt(10))
+    decryptSalt = salted.decode('utf-8')
+    password = decryptSalt
+
+
+    token = jwt.encode(
+            {
+                'username': username,
+                'password': password
+            }, JWT_SECRET, algorithm="HS256")
+
+    db.execute(sql.SQL("INSERT INTO users (token, username, password) VALUES (%s, %s, %s);"), (token, username,password))
+    db.close()
+
+    global_db_con.commit()
+
+    return("true")
+
+
+
+
+
 
 # Creates User Account
 @app.route('/create', methods=['POST'])
@@ -58,6 +99,11 @@ def createAccount():
 
         return(f"An account with {username} is already taken")
 
+    salted = bcrypt.hashpw( bytes(password,  'utf-8' ) , bcrypt.gensalt(10))
+    decryptSalt = salted.decode('utf-8')
+    password = decryptSalt
+
+
     token = jwt.encode(
             {
                 'username': username,
@@ -70,6 +116,36 @@ def createAccount():
     global_db_con.commit()
 
     return(f"Successfully created account. Username = {username} and token = {token}")
+
+
+# Authenticates a User and returns jwt
+@app.route('/auth', methods=['POST'])
+def authAccount():
+    username = request.form['username']
+    password = request.form['password']
+    
+    #data = request.get_json()
+    #username = data['username']
+    #password = data['password']
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM users WHERE username = %s;"),(username,))
+    userPass = db.fetchone()
+    db.close()
+
+    if userPass != None:
+
+        tempPass = bytes(userPass[2], 'utf-8')
+        if bcrypt.checkpw(bytes(password, 'utf-8'), tempPass):
+            return(f"Account active. JWT token = {userPass[0]}")
+
+        return("Invalid Credentials: Password")
+
+    return("Invalid Credentials: Username")
+
+
+
+
 
 
 #Checks if credentials are correct
@@ -92,8 +168,9 @@ def login():
         return "false"
 
     else:
-        if password != userPass[2]:
-            return "true"
+        tempPass = bytes(userPass[2], 'utf-8')
+        if bcrypt.checkpw(bytes(password, 'utf-8'), tempPass):
+            return str(userPass[0])
         else:
             return "false"
 
@@ -129,6 +206,120 @@ def getBooks():
         books.append({"ISBN": book[0], "Title": book[1], "Author": book[2], "Price": book[3] + " USD"})
 
     return jsonify(books)
+
+
+# Purchase book entry into database
+@app.route('/purchase', methods=['POST'])
+def purchaseBook():
+
+    userN = request.form['username']
+    token = request.form['jwt']
+    title = request.form['title']
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM users WHERE token = %s;"),(token,))
+    userInfo = db.fetchone()
+    db.close()
+
+    if(userInfo == None):
+        return("Invalid Token") 
+
+
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM books WHERE title = %s;"),(title,))
+    buy = db.fetchone()
+    db.close()
+
+    if buy == None:
+        return "false"
+    
+    price = buy[3]
+    buyDate = datetime.datetime.now()
+
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM purchases WHERE title = %s;"),(title,))
+    tempCheck = db.fetchone()
+    db.close()
+
+    if tempCheck != None:
+        return "You already bought this!"
+
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("INSERT INTO purchases (username, title, price, date) VALUES (%s, %s, %s, %s);"), (userN, title, price, buyDate))
+    db.close()
+
+    global_db_con.commit()
+
+    return "Successfully purchased!"
+
+
+# Purchase book entry into database
+@app.route('/sell', methods=['POST'])
+def sellBook():
+
+    userN = request.form['username']
+    token = request.form['jwt']
+    title = request.form['title']
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM users WHERE token = %s;"),(token,))
+    userInfo = db.fetchone()
+    db.close()
+
+    if(userInfo == None):
+        return("Invalid Token")
+
+
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM purchases WHERE title = %s;"),(title,))
+    tempCheck = db.fetchone()
+    db.close()
+
+    if tempCheck == None:
+        return "You do not own this!"
+
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("DELETE FROM purchases WHERE title = %s"), (title,))
+    db.close()
+
+    global_db_con.commit()
+
+    return "Successfully sold!"
+
+
+
+
+
+    
+
+# Looks up history of purchases
+@app.route('/history', methods=['POST'])
+def purchaseHistory():
+
+    userN = request.form['username']
+
+
+    db = global_db_con.cursor()
+    db.execute(sql.SQL("SELECT * FROM purchases WHERE username = %s;"),(userN,))
+    historyDB = db.fetchall()
+    db.close() 
+    
+
+
+    purchaseHistory = []
+
+
+    for purchase in historyDB:
+        purchaseHistory.append({"Username": purchase[0], "Title": purchase[1], "Price": purchase[2],"Date": purchase[3]})
+
+    return jsonify(purchaseHistory)
+
+
 
 
 app.run(host='0.0.0.0', port=80)
